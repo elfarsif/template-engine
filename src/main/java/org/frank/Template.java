2983a53270176e8e150a49eacd266bb8b2b0021f
elfarsif <f.n.elfarsi@gmail.com>
2024-07-18 15:05:37 -0400

    GREEN : added template class attributes to get functionality

--- src/main/java/org/frank/Template.java
+++ src/main/java/org/frank/Template.java
@@ -1,15 +1,20 @@
 package org.frank;
 
 public class Template {
+    private String templateString;
+    private String value;
+    private String variable;
 
     public Template(String templateString) {
+        this.templateString = templateString;
     }
 
-    public void set(String name, String reader) {
-
+    public void set(String variable, String value) {
+        this.value = value;
+        this.variable = variable;
     }
 
     public String evaluate() {
-        return "Hello, Reader";
+        return "Hello, "+value;
     }
 }
