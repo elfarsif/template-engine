c0ce44af6ce779048500c0f3d19c1fd1a1cf3d04
elfarsif <f.n.elfarsi@gmail.com>
2024-07-18 22:51:10 -0400

    REFACTOR : removed redundant tests and added asserting template to helper function

--- src/test/java/TemplateTest.java
+++ src/test/java/TemplateTest.java
@@ -1,45 +1,33 @@
 import org.frank.Template;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 public class TemplateTest {
-    @Test
-    void evaluateTemplateWithOneVariable(){
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
-        assertThat(template.evaluate()).isEqualTo("Hello, Reader");
-    }
+    Template template;
 
-    @Test
-    void evaluateTemplateWithDifferentVariable(){
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Frank");
-        assertThat(template.evaluate()).isEqualTo("Hello, Frank");
-    }
-
-    @Test
-    void evaluateWithDifferentTemplate(){
-        Template template = new Template("Hi, ${name}");
-        template.set("name", "Frank");
-        assertThat(template.evaluate()).isEqualTo("Hi, Frank");
-    }
-
-    @Test
-    void multipleVariables(){
-        Template template = new Template("${one}, ${two}, ${three}");
+    @BeforeEach
+    void givenTemplateWithMultipleVaribles(){
+        template = new Template("${one}, ${two}, ${three}");
         template.set("one", "1");
         template.set("two", "2");
         template.set("three", "3");
-        assertThat(template.evaluate()).isEqualTo("1, 2, 3");
     }
 
     @Test
-    public void unknownVariablesAreIgnored() throws Exception {
-        Template template = new Template("Hello, ${name}");
-        template.set("name", "Reader");
+    void itShouldReplaceMultipleVariables(){
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void itShouldIgnoreUnknownVariables() throws Exception {
         template.set("doesnotexist", "Hi");
-        assertThat(template.evaluate()).isEqualTo("Hello, Reader");
+        assertTemplateEvaluatesTo("1, 2, 3");
+    }
+
+    private void assertTemplateEvaluatesTo(String expected){
+        assertThat(template.evaluate()).isEqualTo(expected);
     }
 }
