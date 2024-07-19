75b3642a60ac145e6db4dd470f8b1550db7f4380
elfarsif <f.n.elfarsi@gmail.com>
2024-07-18 23:36:17 -0400

    REFACTOR : extracted method to replace variables and check for missing variables

--- src/main/java/org/frank/Template.java
+++ src/main/java/org/frank/Template.java
@@ -18,14 +18,25 @@ public class Template {
     }
 
     public String evaluate() {
+        String evaluatedTemplate = replaceVariables();
+        checkForMissingValues(evaluatedTemplate);
+        return evaluatedTemplate;
+    }
+
+    private String replaceVariables(){
         String result = templateText;
         for (Entry<String, String> entry : variables.entrySet()) {
             String regex = "\\$\\{" + entry.getKey() + "\\}";
             result = result.replaceAll(regex, entry.getValue());
         }
+        return result;
+    }
+
+    private void checkForMissingValues(String result){
         if (result.matches(".*\\$\\{.+\\}.*")) {
             throw new MissingValueException();
         }
-        return result;
     }
+
+
 }
