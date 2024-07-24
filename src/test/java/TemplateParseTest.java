1b9ba77a677fb2c8914489be4a90346e7deaa3c6
elfarsif <f.n.elfarsi@gmail.com>
2024-07-24 14:14:44 -0400

    REFACTOR : extracted calling TemplateParse.parse() as well as asserting Segments into a seperate function

--- src/test/java/TemplateParseTest.java
+++ src/test/java/TemplateParseTest.java
@@ -2,18 +2,36 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TemplateParseTest {
 
     @Test
     public void emptyTemplateRendersAsEmptyString() throws Exception {
-        TemplateParse parse = new TemplateParse();
-        List<String> segments = parse.parse("");
-        assertThat(segments.size())
-                .as("Number of segments")
-                .isEqualTo(1);
-        assertThat(segments.get(0))
-                .isEqualTo("");
+        List<String> segments = parse("");
+        assertSegments(segments, "");
     }
+
+    @Test
+    public void templateWithOnlyPlainText() throws Exception {
+        List<String> segments = parse("plain text only");
+        assertSegments(segments, "plain text only");
+
+    }
+
+    private List<String> parse(String templateText) {
+        return new TemplateParse().parse(templateText);
+    }
+
+    private void assertSegments(List<String> actual, String... expected) {
+        assertThat(expected.length)
+                .as("Number of Segments dont match")
+                .isEqualTo(actual.size());
+        assertThat(Arrays.asList(expected))
+                .isEqualTo(actual);
+
+    }
+
+
 }
